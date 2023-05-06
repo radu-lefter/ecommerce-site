e8560b816ce264c04a035173acb8b3f10e7c8ca5
Radu Lefter <lefter_radu@yahoo.com>
2023-05-07 00:03:27 +0100

    created basic navbar an footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Home } from './pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Footer, Navbar } from './components';
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+
+import styled from 'styled-components'
+
+const Nav = () => {
+  return <NavContainer>hello world</NavContainer>
+}
+
+const NavContainer = styled.nav``
+
+export default Nav
